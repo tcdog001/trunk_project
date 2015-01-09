68fbfdb98236754934d065ea25e75eb5c89bebf9
liuhuijun <liuhuijun@debian6.(none)>
2015-01-09 10:57:13 -0500

    modify for 1.0.6

--- trunk/files/sbin/network_firewall_default.sh
+++ trunk/files/sbin/network_firewall_default.sh
@@ -3,7 +3,7 @@
 cp /rom/etc/config/network /etc/config/network
 cp /rom/etc/config/firewall /etc/config/firewall
 
-/etc/init.d/network restart > /dev/null 2>&1
+set_evdo_service.sh > /dev/null 2>&1
 /etc/init.d/firewall restart > /dev/null 2>&1
 
 
