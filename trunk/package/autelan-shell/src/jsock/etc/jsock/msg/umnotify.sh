3ec5f106927b7503f796d1205bd0c5a1c61131ed
ljf10000 <l_j_f@163.com>
2015-01-26 16:40:36 +0800

    add jsock/msg/umnotify.sh umnotify.system.cb

---
+++ trunk/package/autelan-shell/src/jsock/etc/jsock/msg/umnotify.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+	
+. ${__ROOTFS__}/etc/jsock/jsock.in
+
+#
+#$1:event
+#$2:mac
+#$3:ip
+#
+main() {
+	jsock_md_send_check || {
+		return ${e_bad_board}
+	}
+
+	local event="$1"
+	local mac="$2"
+	local ip="$3"
+
+	local json=$(printf '{"event":"%s","mac":"%s","ip":"%s"}' \
+		"${event}" \
+		"${mac}" \
+		"${ip}")
+
+	${__ROOTFS__}/etc/jsock/jmsg.sh syn umnotify "${json}"
+}
+
+main "$@"
