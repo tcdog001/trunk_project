9d652f05261efbb00b45adc36fa531afbea0950d
liuhj <liuhuijun@debian.suns>
2015-02-01 22:50:17 -0500

    modify acc_power_off.sh\drop_off.sh\get_time.sh for onoff

--- trunk/files/sbin/acc_power_off.sh
+++ trunk/files/sbin/acc_power_off.sh
@@ -8,13 +8,6 @@ get_onoff_log() {
 	local ontime=$( cat ${ontime_file} |sed -n '$p' )       
 	local offtime=$(get_time)
 
-	local line=$( grep -n "" ${ontime_file} |wc -l )
-	local del_line=$(awk 'BEGIN{printf("%d",'$line'-'2')}')
-
-	if [[ ${line} -gt 2 ]];then
-		sed -e "1,${del_line}"d ${ontime_file} -i 2>/dev/null
-	fi
-
 	write_ap_onoff_reason ${ontime} ${offtime} ${file_path}/ap-on-off-${offtime} ACC-OFF	
 }
 
--- trunk/files/sbin/drop_off.sh
+++ trunk/files/sbin/drop_off.sh
@@ -10,14 +10,14 @@ get_onoff_log() {
 	local line=$( grep -n "" ${file_path}/${file_on} |wc -l )
 	local del_line=$(awk 'BEGIN{printf("%d",'$line'-'2')}')
 	
+	if [[ ${line} -gt 2 ]];then                                     
+		sed -e "1,${del_line}"d ${file_path}/${file_on} -i 2>/dev/null
+	fi
+
 	if [[ -e ${file_path}/acc_off.txt ]];then                              
 		rm -rf ${file_path}/acc_off.txt;
 		return 0;                             
 	else 
-		if [[ ${line} -gt 2 ]];then                                     
-			sed -e "1,${del_line}"d ${file_path}/${file_on} -i 2>/dev/null
-		fi
-		
 		write_ap_onoff_reason ${ontime} ${offtime} ${file_path}/ap-on-off-${offtime} DROP-OFF	
 	fi
 }
