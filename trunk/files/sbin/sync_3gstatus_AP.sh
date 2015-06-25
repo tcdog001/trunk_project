f997ee714cf74ac196951b3f81c5d574b0f5f8a1
liuhuijun <liuhuijun@e830.(none)>
2015-06-25 15:45:34 +0800

    disable wifidog and tc

--- trunk/files/sbin/sync_3gstatus_AP.sh
+++ trunk/files/sbin/sync_3gstatus_AP.sh
@@ -28,9 +28,9 @@ status_3g() {
         if [[ -z ${inter_evdo} ]];then
                 echo ${DOWN} >${FILE}
                 ((status_i++))
-                if [[ ${WFDG_stat} -eq 1 ]];then
-                        ${WIFIDOG} start >/dev/null 2>&1
-                fi
+                #if [[ ${WFDG_stat} -eq 1 ]];then
+                #        ${WIFIDOG} start >/dev/null 2>&1
+                #fi
                 if [[ ${status_i} -ge 9 ]];then
                         killall check_3gdown_result.sh 2>/dev/null
                         check_3gdown_result.sh &
@@ -41,9 +41,9 @@ status_3g() {
                 echo ${UP} >${FILE}
                 status_i=0
                 echo ${evdo_ip} >${file_evdo_ip}
-                if [[ ${WFDG_stat} -eq 0 ]];then
-                        ${WIFIDOG} stop >/dev/null 2>&1
-                fi
+                #if [[ ${WFDG_stat} -eq 0 ]];then
+                #        ${WIFIDOG} stop >/dev/null 2>&1
+                #fi
         fi
         rm -rf ${PS_WFDG} 2>/dev/null
 }
